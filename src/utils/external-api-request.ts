38910b3c46afffe54d9d4dfd1f9f2ac5f21ce50f
C-Macargo <caio.camargos97@gmail.com>
2023-06-11 12:42:46 -0300

    feat: external google books api request

---
+++ src/utils/external-api-request.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default async function searchBooks(query: string) {
+	try {
+		const fields =
+			"items(volumeInfo/title,volumeInfo/authors,volumeInfo/publishedDate,volumeInfo/imageLinks,volumeInfo/description,volumeInfo/subtitle,volumeInfo/language)";
+		const response = await axios.get(
+			"https://www.googleapis.com/books/v1/volumes",
+			{
+				params: {
+					q: query,
+					fields,
+					key: process.env.API_KEY,
+					"partial.fieldsets": fields,
+				},
+			}
+		);
+		return response.data;
+	} catch (error) {
+		console.error(`Error occurred: ${error}`);
+	}
+}
