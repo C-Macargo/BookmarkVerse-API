0193d140d69aa73f5305d3867b7a16a0f3c79fc8
C-Macargo <caio.camargos97@gmail.com>
2023-06-22 14:16:28 -0300

    fix: adding correct status code error message

--- src/middlewares/schema-validation-middleware.ts
+++ src/middlewares/schema-validation-middleware.ts
@@ -1,14 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
-import httpStatus from 'http-status';
-import { Schema } from 'joi';
+import { Request, Response, NextFunction } from "express";
+import httpStatus from "http-status";
+import { Schema } from "joi";
 
 export function validateSchema(schema: Schema) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const { error } = schema.validate(req.body, { abortEarly: false });
-        if (error) {
-            const errorMessage = error.details[0].message;
-            return res.status(httpStatus.UNPROCESSABLE_ENTITY).json({ error: errorMessage });
-        }
-        next();
-    };
+	return (req: Request, res: Response, next: NextFunction) => {
+		const { error } = schema.validate(req.body, { abortEarly: false });
+		if (error) {
+			const errorMessages = error.details.map((detail) => detail.message);
+			return res
+				.status(httpStatus.BAD_REQUEST)
+				.json({ errors: errorMessages });
+		}
+		next();
+	};
 }
